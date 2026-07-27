92999ef386aa748957651e78cc3ae8e6085047e3
Richisoft-KOT <Windows.98.1998@yandex.ru>
2026-07-27 20:55:22 +0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -6,13 +6,12 @@ var SUPABASE_KEY = 'sb_publishable_1Zxs_9yRDVRr1DTfyck8WA_v8VUst77';
 var cUser = localStorage.getItem('koto_current_user');
 var cId = parseInt(localStorage.getItem('koto_current_id')) || 0;
 
-// ЖЕСТКАЯ ЗАЩИТА: Автоматический сброс сессии, если плохой дядя подделал ID в консоли смартфона
+// ЖЕСТКАЯ ЗАЩИТА: Автоматический сброс сессии при попытке подменить ID
 if (cUser) {
   var checkName = cUser.toLowerCase();
   if ((checkName === 'richisoft' && cId !== 1) || (checkName === 'тестер' && cId !== 2)) {
     localStorage.clear();
     alert('Система безопасности: Сессия подделана!');
-    // Определяем имя текущего файла, чтобы не гонять по кругу
     var page = window.location.pathname.split("/").pop();
     if (page !== 'login.html' && page !== 'register.html') {
       window.location.href = 'login.html';
@@ -20,12 +19,10 @@ if (cUser) {
   }
 }
 
-// Помощник для проверки, админ ли текущий пользователь
 function isUserAdmin() {
   return (cId === 1 || cId === 2);
 }
 
-// Помощник для получения тега роли
 function getRoleTag() {
   if (cId === 1) return ' [создатель]';
   if (cId === 2) return ' [тест]';
